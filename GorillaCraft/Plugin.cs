a925d85023a6d57a877178e64043f1912bfdaa31
dev <81720436+developer9998@users.noreply.github.com>
2024-12-16 17:36:07 -0330

    hook tilla

--- GorillaCraft/Plugin.cs
+++ GorillaCraft/Plugin.cs
@@ -2,11 +2,10 @@
 using Bepinject;
 using GorillaCraft.Models;
 using HarmonyLib;
-using Utilla;
 
 namespace GorillaCraft
 {
-    [ModdedGamemode, BepInPlugin(Constants.GUID, Constants.Name, Constants.Version), BepInIncompatibility("dev.devminecraftinternal"), BepInIncompatibility("org.iidk.gorillatag.iimenu")]
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version), BepInDependency("dev.tillahook", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static Watchable<bool> Allowed { get; private set; }
@@ -18,18 +17,9 @@ namespace GorillaCraft
             Zenjector.Install<MainInstaller>().OnProject().WithConfig(Config).WithLog(Logger);
 
             Harmony.CreateAndPatchAll(typeof(Plugin).Assembly, Constants.GUID);
-        }
 
-        [ModdedGamemodeJoin]
-        public async void OnJoin()
-        {
-            Allowed.value = true;
-        }
-
-        [ModdedGamemodeLeave]
-        public async void OnLeave()
-        {
-            Allowed.value = false;
+            TillaHook.TillaHook.OnModdedJoin += (string gameMode) => Allowed.value = true;
+            TillaHook.TillaHook.OnModdedLeave += (string gameMode) => Allowed.value = false;
         }
     }
 }
